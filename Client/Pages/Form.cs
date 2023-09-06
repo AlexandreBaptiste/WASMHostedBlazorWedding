4f7af8adcd99761da4411fb877e5128e578d2321
AlexandreBaptiste <alexandre.baptiste.etu@gmail.com>
2023-09-06 18:45:28 +0200

    Ajout de la page formulaire et début d'apprentissage sur les formulaire Blazor

---
+++ Client/Pages/Form.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Wedding.Interfaces;
+using Wedding.Shared.Models;
+
+namespace Wedding.Client.Pages
+{
+    public partial class Form : ComponentBase
+    {
+        [Inject]
+        public IguestsService? guestsService { get; set; }
+
+        [Inject]
+        public ILogger<Guests>? Logger { get; set; }
+
+        public List<Guest> guestList { get; set; } = new List<Guest>();
+
+        public Guest? guest { get; set; }
+
+        public string LastSubmitResult = string.Empty;
+
+        public EditContext? EditContext { get; set; }  
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (guestsService != null)
+            {
+                var guests = await guestsService.Getguests();
+                guestList = guests != null && guests.Any() ? guests.ToList() : new List<Guest>();
+            }
+
+            if(guestList != null)
+            {
+                guest = guestList.FirstOrDefault();
+                EditContext = new EditContext(guestList);
+            }
+        }
+
+        public void ValidFormSubmitted(EditContext editContext)
+        {
+            LastSubmitResult = editContext.Validate() ? "Success - form was valid" : "Failure - form was invalid";
+        }
+    
+    }
+}
